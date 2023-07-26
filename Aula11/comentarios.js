599d752f5f453b3d17f5771989e33322956967d0
NicoleStarling <nicolesstarling02@gmail.com>
2023-07-26 16:10:09 -0300

    Exercício com números

---
+++ Aula11/comentarios.js
@@ -0,0 +1,15 @@
+// Escrever um comentário
+console.log('Olá mundo!');//comentário
+console.log('Linha 1')
+
+/*é usado para comentários longos
+que usam mais de uma linha
+o outro tipo são para comentários curtos
+que usam apenas uma linha ou parte de 
+linha */
+
+/**
+ * Título
+ * Texto
+ * Conclusão
+ */
