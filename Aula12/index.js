2695e2b5f97178b1c0bc2616393493dd2a67d402
NicoleStarling <nicolesstarling02@gmail.com>
2023-07-26 16:10:54 -0300

    Trocando valor de variáveis

---
+++ Aula12/index.js
@@ -0,0 +1,5 @@
+console.log('Olá mundo!');// CMD eu consigo executar o comando também.
+//Um comentário
+
+console.log('Olá mundo!');
+console.log('Este trecho será exibido no console do navegador');
