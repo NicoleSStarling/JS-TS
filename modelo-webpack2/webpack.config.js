5bdf1b68015692a2526aeb1a4536ad558a569eb0
NicoleStarling <nicolesstarling02@gmail.com>
2023-07-26 17:10:10 -0300

    Modelo para criação de projetos

---
+++ modelo-webpack2/webpack.config.js
@@ -0,0 +1,59 @@
+/*const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+       path: path.resolve(__dirname, 'public', 'assets', 'js'),
+       filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            exclude: /node_modules/,
+            test: /\.js$/,
+            use: {
+                loader: 'babel_loader',
+                options: {
+                    presets: ['@babel/env']
+                }
+            }
+        }]
+    },
+    devtool: 'source-map'//te informa a linha exata onde ocorreu o erro.
+};*/
+
+const path = require('path'); // CommonJS
+ 
+module.exports = {
+  mode: 'production',
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'public', 'assets', 'js'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      exclude: /node_modules/,
+      test: /\.js$/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: {
+                  esmodules: true,
+                },
+              },
+            ],
+          ],
+        }
+      }
+    }, {
+      test: /\.css$/,
+      use:['style-loader', 'css-loader']
+    }]
+  },
+  devtool: 'source-map'
+};
