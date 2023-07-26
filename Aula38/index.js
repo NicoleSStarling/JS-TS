10ad49733242072669289fb27d31afceefcd3213
NicoleStarling <nicolesstarling02@gmail.com>
2023-07-26 16:36:53 -0300

    Operadores lógicos

---
+++ Aula38/index.js
@@ -0,0 +1,13 @@
+/*
+Operadores lógicos
+&& -> AND -> E
+|| -> OR -> OU
+! -> NOT -> NÃO
+*/
+
+console.log(true && true);//Todas as condições tem que ser verdadeiras para que retorde verdadeira, se alguma condição for falsa retorna false.,
+console.log(true && true && false); //Mais um exempro de AND.
+console.log(true || false); //Se uma das condições for atendida retorna true, se ambas forem atendidas retorna true, somente se todas forem falsas retorna false.
+console.log(false || false); //Mais um exemplo do OU.
+console.log(! true); //Negando true ele vira false, ou negando o false ele vira verdadeiro.
+console.log(! false); //Mais um exempro de NOT.
