f191986888b793b64f8f6edd33c3545e6f85e007
NicoleStarling <nicolesstarling02@gmail.com>
2023-07-26 16:52:35 -0300

    Objeto MAP()

---
+++ Aula101/index.js
@@ -0,0 +1,21 @@
+//Objeto map()
+
+const pessoas = [
+    {id: 3, nome: 'Nicole'},
+    {id: 2, nome: 'Pedro'},
+    {id:1, nome: 'Luiz'},
+];
+
+/* const novasPessoas = {};
+for (const pessoa of pessoas){
+    const {id} = pessoa;
+    novasPessoas[id] = {...pessoa};
+} */
+
+const novasPessoas = new Map();
+for (const pessoa of pessoas){
+    const {id} = pessoa;
+    novasPessoas.set(id, {...pessoa});
+}
+console.log(novasPessoas);
+console.log(novasPessoas.get(2));
