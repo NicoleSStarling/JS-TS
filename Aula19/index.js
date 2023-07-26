d1b3bab24548f51fcd04774b838cf2b9222e67fd
NicoleStarling <nicolesstarling02@gmail.com>
2023-07-26 16:17:16 -0300

    Exercício de lógica

---
+++ Aula19/index.js
@@ -0,0 +1,16 @@
+//String, number, undefined, null, boolean, symbol.
+
+const nome = 'Luiz'; //String
+const nome1 = "Luiz"; //String
+const nome2 = `Luiz`; //String
+const num1 = 10; //number
+const num2 = 10.52; //number com numero flutuante
+let nomeAluno; //Declarei mas não inicializei, undefined = não aponta para local nenhum na memoria.
+let sobreNomeAluno = null; //Valor nulo, não aponta para local nenhum na memoria. Null é diferente de undefined.
+//const sempre tem que ter um valor então quando quero somente declarar a variável tenho que usar let.
+const boolean = true; //Boolean somente pode ser true ou false.
+//typeof é usado para saber qual o tipo de dado.
+console.log(typeof nome, nome); //tipo da variável e o valor.
+
+
+
