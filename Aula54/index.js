c5fd8e002aa84c23ab655fbc2120b4033a1fa592
NicoleStarling <nicolesstarling02@gmail.com>
2023-07-26 16:41:55 -0300

    DOM e arvore DOM

---
+++ Aula54/index.js
@@ -0,0 +1,7 @@
+//AULA SOBRE DOM, Modelo de Objeto de documento.
+/*
+No navegador o objeto global é o Window (pai de todos os elementos no html).
+Arvore do DOM, significa que os elementos são filhos de outros elementos e assim por diante.
+Exemplo do html, o body é filho do HTML, o HTML é filho do document, e o document é filho do window.
+DOM se refere ao navegador e a uma interface de objetos que me permite manipular o elemento dentro da pagina.
+ */
