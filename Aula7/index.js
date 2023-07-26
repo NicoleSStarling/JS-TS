1c66f73b3b612d2b272397c604076a307c4b05ec
NicoleStarling <nicolesstarling02@gmail.com>
2023-07-26 16:06:06 -0300

    Iniciando JS

---
+++ Aula7/index.js
@@ -0,0 +1,4 @@
+console.log('Meu nome é "Nicole". Estou aprendendo JS as', 10, 'da manhã.'); //String
+console.log("Nicole Starling");
+console.log(123456, 15.85); //Number
+console.log(35, 15.85, 'Nicole');
