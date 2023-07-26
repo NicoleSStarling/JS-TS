de9e0db4345b4e85983477f1a00cdbd0a239342a
NicoleStarling <nicolesstarling02@gmail.com>
2023-07-26 16:17:08 -0300

    Exercício de lógica

---
+++ Aula18/index.js
@@ -0,0 +1,14 @@
+//Diferença entre var e let
+var nome ='Luiz';
+var nome ='Otávio';
+
+console.log(nome);
+
+// Toda vez que eu usar o nome da variavel declarada por var eu estou redeclarando a mesma e mudando o seu valor.
+
+let idade = 28;
+idade = 29;
+
+console.log(idade);
+
+// Quando quero redeclarar o valor da variavel que foi lançada como let eu apenas coloco no nome da variavel com o novo valor.
